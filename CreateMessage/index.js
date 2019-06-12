b33cb762b88d723e675a10b78ba245b3dda64af8
Doug Chamberlain <d.chamberlain@nextech.com>
2019-06-12 17:33:18 -0400

    all the things

--- CreateMessage/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (context, req) {  
-    context.bindings.cosmosDBMessage = req.body;
-    context.bindings.signalRMessages = [{
-        "target": "newMessage",
-        "arguments": [req.body]
-    }];
-    context.done();
-};
--- GetMessages/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (context, req, messages) {
-    context.res.body = messages;
-    context.done();
-};
